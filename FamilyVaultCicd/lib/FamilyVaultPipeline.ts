0400cb83151348434cd8ca50972c97c1d2782c29
Daniel McCarthy <dmacl0l1992@gmail.com>
2024-04-27 13:43:39 +1000

    update pipeline to conditionally deploy when required

--- FamilyVaultCicd/lib/FamilyVaultPipeline.ts
+++ FamilyVaultCicd/lib/FamilyVaultPipeline.ts
@@ -43,6 +43,9 @@ export class FamilyVaultPipeline extends cdk.Stack {
             resources: ['arn:aws:iam::058264330237:role/cicd-role-test']
         }));
 
+        //TODO - maybe need a more comprehensive fix to ensure pipeline doesnt attempt to run cdk
+        //this would probably not even be a problem once we can prevent rust lambda functions from compiling on every cdk synth/deploy/diff
+        //need to move all functional files into a single folder ( lib probably ), if changes madde outside lib, dont redeploy
         const buildSpec = BuildSpec.fromObject({
             version: '0.2',
             phases: {
@@ -53,13 +56,25 @@ export class FamilyVaultPipeline extends cdk.Stack {
                         'export AWS_ACCESS_KEY_ID=$(echo $TEMP_ROLE | jq -r .Credentials.AccessKeyId)',
                         'export AWS_SECRET_ACCESS_KEY=$(echo $TEMP_ROLE | jq -r .Credentials.SecretAccessKey)',
                         'export AWS_SESSION_TOKEN=$(echo $TEMP_ROLE | jq -r .Credentials.SessionToken)',
+                        // Check for changes in the FamilyVaultCdk directory
+                        'CHANGES_OUTSIDE_TEST=$(git diff --name-only origin/main HEAD -- FamilyVaultCdk/ | grep -v "FamilyVaultCdk/test/")',
+                        'if [ -n "$CHANGES_OUTSIDE_TEST" ]; then',
+                        '  export FAMILY_VAULT_CDK_CHANGED="true"',
+                        'else',
+                        '  echo "Changes only in FamilyVaultCdk/test folder, skipping deploy."',
+                        '  export FAMILY_VAULT_CDK_CHANGED="false"',
+                        'fi'
                     ]
                 },
                 build: {
                     commands: [
-                        'cd FamilyVaultCdk',
-                        'npm install',
-                        'npx cdk deploy --all --require-approval never'
+                        'if [ "$FAMILY_VAULT_CDK_CHANGED" == "true" ]; then',
+                        '  cd FamilyVaultCdk',
+                        '  npm install',
+                        '  npx cdk deploy --all --require-approval never',
+                        'else',
+                        '  echo "No changes in FamilyVaultCdk since last commit. Skipping deploy."',
+                        'fi'
                     ]
                 }
             }
@@ -77,7 +92,7 @@ export class FamilyVaultPipeline extends cdk.Stack {
             actionName: 'test_deploy',
             project: testBuildProject,
             input: sourceOutput,
-            outputs: [buildOutput], //optional, delete probably
+            outputs: [buildOutput], 
         });
 
         pipeline.addStage({
@@ -86,7 +101,7 @@ export class FamilyVaultPipeline extends cdk.Stack {
         });
 
         pipeline.addStage({
-            stageName: 'Build',
+            stageName: 'TestDeploy',
             actions: [TestDeployAction],
         });
     }
